a05cfaa037f4118865f8a5557d14c875dcd7ec63
Kevin Petit <74395438+kvnpetit@users.noreply.github.com>
2026-01-23 14:23:06 +0100

    Resolves 3

--- lib/game.dart
+++ lib/game.dart
@@ -1,17 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:sudoku_api/sudoku_api.dart';
 import 'package:sudoku_starter/inner_grid.dart';
 
 class Game extends StatefulWidget {
   const Game({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -20,16 +12,19 @@ class Game extends StatefulWidget {
 }
 
 class _GameState extends State<Game> {
-  int _counter = 0;
+  Puzzle? puzzle;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    _generatePuzzle();
+  }
+
+  void _generatePuzzle() {
+    PuzzleOptions puzzleOptions = PuzzleOptions(patternName: "winter");
+    puzzle = Puzzle(puzzleOptions);
+    puzzle!.generate().then((_) {
+      setState(() {});
     });
   }
 
@@ -59,17 +54,12 @@ class _GameState extends State<Game> {
                 decoration: BoxDecoration(
                   border: Border.all(color: Colors.blueAccent),
                 ),
-                child: InnerGrid(boxSize: boxSize),
+                child: InnerGrid(boxSize: boxSize, puzzle: puzzle, blockIndex: x),
               );
             }),
           ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- lib/inner_grid.dart
+++ lib/inner_grid.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:sudoku_api/sudoku_api.dart';
 
 class InnerGrid extends StatelessWidget {
   final double boxSize;
+  final Puzzle? puzzle;
+  final int blockIndex;
 
-  const InnerGrid({Key? key, required this.boxSize}) : super(key: key);
+  const InnerGrid({
+    Key? key,
+    required this.boxSize,
+    required this.puzzle,
+    required this.blockIndex,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -11,12 +19,19 @@ class InnerGrid extends StatelessWidget {
 
     return GridView.count(
       crossAxisCount: 3,
-      children: List.generate(9, (x) {
+      children: List.generate(9, (y) {
+        int? value = puzzle?.board()?.matrix()?[blockIndex][y].getValue();
+
         return Container(
           width: innerBoxSize,
           height: innerBoxSize,
           decoration: BoxDecoration(
             border: Border.all(color: Colors.black, width: 0.3),
+          ),
+          child: Center(
+            child: Text(
+              value != null && value != 0 ? value.toString() : '',
+            ),
           ),
         );
       }),
